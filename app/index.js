a769c2358796fd2986e6f6c9eca28072bde2e1d0
kwamen98 <derrickdadson14@gmail.com>
2024-05-24 19:40:09 +0000

    done with some functions of the home page

--- app/index.js
+++ app/index.js
@@ -1,10 +1,41 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,SafeAreaView,ScrollView,Text} from 'react-native'
+import {Stack,useRouter} from 'expo-router'
+import {COLORS,icons,images,SIZES} from '../constants'
+
+import {
+    Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome
+} from '../components'
 
 export default function Home() {
+    const router=useRouter();
   return (
-    <View>
-        <Text>Home Page</Text>
-    </View>
+    <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
+       <Stack.Screen
+        options={{
+            headerStyle:{backgroundColor:COLORS.lightWhite},
+            title:''
+            ,headerShadowVisible:false,
+            headerLeft:()=>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+            ),
+            headerRight: ()=>(
+                <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>
+            )
+        }}
+        
+       />
+       <ScrollView showsVerticalScrollIndicator={false}>
+
+
+            <View style={{flex:1,padding:SIZES.medium}}>
+                <Welcome/>
+                <Popularjobs/>
+                <Nearbyjobs/>
+            </View>
+
+
+       </ScrollView>
+    </SafeAreaView>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput,TouchableOpacity,Image } from 'react-native'
+import {useRouter} from 'expo-router';
+import {icons,SIZES} from '../../../constants'
 
 import styles from './welcome.style'
 
 const Welcome = () => {
+  const router=useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+          <Text style={styles.userName}>Hello Adiran</Text>
+          <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=''
+            onChange={()=>{}}
+            placeholder='What are you looking for ?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+          <Image
+            source = {icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          
+          />
+
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
